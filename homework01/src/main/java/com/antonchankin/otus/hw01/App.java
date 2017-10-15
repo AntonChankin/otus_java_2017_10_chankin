bab8f4cf7882ace493aa739f0556e94378f299dd
Anton Chankin <this@antonchankin.com>
2017-10-15 16:57:10 +0300

    Homework01 Поправил pom и досбавил Hello World

---
+++ homework01/src/main/java/com/antonchankin/otus/hw01/App.java
@@ -0,0 +1,10 @@
+package com.antonchankin.otus.hw01;
+
+/**
+ * Hello World to test JAR
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Otus!");
+    }
+}
