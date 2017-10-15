5ab166446e44edc8911e06b4cf7357da97123e46
Anton Chankin <this@antonchankin.com>
2017-10-15 18:56:40 +0300

    Homework01 Добавил в App зависимость для проверки что JAR собирается с зависимостями

--- homework01/src/main/java/com/antonchankin/otus/hw01/App.java
+++ homework01/src/main/java/com/antonchankin/otus/hw01/App.java
@@ -1,10 +1,13 @@
 package com.antonchankin.otus.hw01;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Hello World to test JAR
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Otus!");
+        System.out.println("Is string \"Hello Otus!\" empty? " + StringUtils.isEmpty("Hello Otus!"));
+
     }
 }
