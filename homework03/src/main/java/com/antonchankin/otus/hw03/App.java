51d75e0e29789ce2af0e6a36ef65249ef05d4ff1
Anton Chankin <this@antonchankin.com>
2017-10-30 10:04:55 +0300

    Homerowk 03 Inits

---
+++ homework03/src/main/java/com/antonchankin/otus/hw03/App.java
@@ -0,0 +1,20 @@
+package com.antonchankin.otus.hw03;
+
+import java.lang.management.ManagementFactory;
+
+/**
+ * Memory Measurement App
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("pid: " + ManagementFactory.getRuntimeMXBean().getName());
+
+    }
+
+
+
+
+}
