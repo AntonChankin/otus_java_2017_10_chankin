9bc3aeaa5c9fe0cc01ee144d27fb3dabf6c9ef9e
Anton Chankin <this@antonchankin.com>
2017-10-31 17:07:19 +0300

    Homework 03 Adds tests for sort and copy

--- homework03/src/test/java/com/antonchankin/otus/hw03/OtusArrayListTest.java
+++ homework03/src/test/java/com/antonchankin/otus/hw03/OtusArrayListTest.java
@@ -2,6 +2,11 @@ package com.antonchankin.otus.hw03;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class OtusArrayListTest extends TestCase {
     public void testSize() throws Exception {
     }
@@ -73,8 +78,26 @@ public class OtusArrayListTest extends TestCase {
     }
 
     public void testSort() throws Exception {
+        OtusArrayList<String> list = new OtusArrayList<>(3);
+        list.add("alpha");
+        list.add("gamma");
+        list.add("beta");
+        Collections.sort(list, (o1, o2) -> -o1.compareToIgnoreCase(o2));
+        assertEquals("beta",list.get(1));
     }
 
     public void testCopy() throws Exception {
+        OtusArrayList<String> list = new OtusArrayList<>(3);
+        list.add("alpha");
+        list.add("gamma");
+        list.add("beta");
+        List<String> target = new ArrayList<>(3);
+        target.add("dummy");
+        target.add("dummy");
+        target.add("dummy");
+        Collections.copy(target, list);
+        assertEquals(list.get(0),target.get(0));
+        assertEquals(list.get(1),target.get(1));
+        assertEquals(list.get(2),target.get(2));
     }
 }
