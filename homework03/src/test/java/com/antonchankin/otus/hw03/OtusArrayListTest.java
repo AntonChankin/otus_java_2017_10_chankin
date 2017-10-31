259c6f3dc6501d8833ed74d82bfca15277050a23
Anton Chankin <this@antonchankin.com>
2017-10-31 11:11:14 +0300

    inits tests

---
+++ homework03/src/test/java/com/antonchankin/otus/hw03/OtusArrayListTest.java
@@ -0,0 +1,80 @@
+package com.antonchankin.otus.hw03;
+
+import junit.framework.TestCase;
+
+public class OtusArrayListTest extends TestCase {
+    public void testSize() throws Exception {
+    }
+
+    public void testIsEmpty() throws Exception {
+    }
+
+    public void testContains() throws Exception {
+    }
+
+    public void testIterator() throws Exception {
+    }
+
+    public void testToArray() throws Exception {
+    }
+
+    public void testToArray1() throws Exception {
+    }
+
+    public void testAdd() throws Exception {
+    }
+
+    public void testRemove() throws Exception {
+    }
+
+    public void testContainsAll() throws Exception {
+    }
+
+    public void testAddAll() throws Exception {
+    }
+
+    public void testAddAll1() throws Exception {
+    }
+
+    public void testRemoveAll() throws Exception {
+    }
+
+    public void testRetainAll() throws Exception {
+    }
+
+    public void testClear() throws Exception {
+    }
+
+    public void testGet() throws Exception {
+    }
+
+    public void testSet() throws Exception {
+    }
+
+    public void testAdd1() throws Exception {
+    }
+
+    public void testRemove1() throws Exception {
+    }
+
+    public void testIndexOf() throws Exception {
+    }
+
+    public void testLastIndexOf() throws Exception {
+    }
+
+    public void testListIterator() throws Exception {
+    }
+
+    public void testListIterator1() throws Exception {
+    }
+
+    public void testSubList() throws Exception {
+    }
+
+    public void testSort() throws Exception {
+    }
+
+    public void testCopy() throws Exception {
+    }
+}
