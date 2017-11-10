560115e87b3a1173b1451556910f40a1e9bc3c57
Anton Chankin <this@antonchankin.com>
2017-11-10 12:13:42 +0300

    Homework 04 adds test app

--- homework04/src/main/java/com/antonchankin/otus/App.java
+++ homework04/src/main/java/com/antonchankin/otus/App.java
@@ -6,8 +6,9 @@ package com.antonchankin.otus;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+        GCListener.installGCMonitoring();
+        Crasher memoryTest = new Crasher();
+        memoryTest.generateOOM();
     }
 }
