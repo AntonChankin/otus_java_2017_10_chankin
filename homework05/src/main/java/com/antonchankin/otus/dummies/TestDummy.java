5ea56c788fa0e7c6a1a12f16df59b68e1fa69d5a
Anton Chankin <this@antonchankin.com>
2017-11-14 13:54:47 +0300

    Homework 05 Implements TestDummy

--- homework05/src/main/java/com/antonchankin/otus/dummies/TestDummy.java
+++ homework05/src/main/java/com/antonchankin/otus/dummies/TestDummy.java
@@ -3,38 +3,38 @@ package com.antonchankin.otus.dummies;
 import com.antonchankin.otus.annotations.After;
 import com.antonchankin.otus.annotations.Before;
 import com.antonchankin.otus.annotations.Test;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class TestDummy {
 
     @Before
-    public boolean beforeMethodAlpha(){
-        boolean isSuccessful = false;
-        return isSuccessful;
+    public void beforeMethodAlpha(){
+        log.info("Before Method Alpha have been called");
     }
 
     @Before
-    public boolean beforeMethodBeta(){
-        boolean isSuccessful = false;
-        return isSuccessful;
+    public void beforeMethodBeta(){
+        log.info("Before Method Beta have been called");
     }
     @Test
     public boolean testMethodAlpha(){
+        log.info("Test Method Alpha have been called");
         boolean isSuccessful = false;
         return isSuccessful;
     }
     @Test
     public boolean testMethodBeta(){
+        log.info("Test Method Beta have been called");
         boolean isSuccessful = false;
         return isSuccessful;
     }
     @After
-    public boolean afterMethodAlpha(){
-        boolean isSuccessful = false;
-        return isSuccessful;
+    public void afterMethodAlpha(){
+        log.info("After Method Alpha have been called");
     }
     @After
-    public boolean afterMethodBeta(){
-        boolean isSuccessful = false;
-        return isSuccessful;
+    public void afterMethodBeta(){
+        log.info("After Method Beta have been called");
     }
 }
