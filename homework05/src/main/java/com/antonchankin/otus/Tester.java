bb19c24f13d36dd771a45758e5fa7ec9a682d74c
Anton Chankin <this@antonchankin.com>
2017-11-14 14:54:09 +0300

    Homework 05 borrows methods from lecture

--- homework05/src/main/java/com/antonchankin/otus/Tester.java
+++ homework05/src/main/java/com/antonchankin/otus/Tester.java
@@ -2,6 +2,12 @@ package com.antonchankin.otus;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Slf4j
 public class Tester {
 
@@ -21,5 +27,30 @@ public class Tester {
         boolean isSuccessful = false;
         //TODO: Method running tests for package
         return isSuccessful;
+    }
+
+    static Object callMethod(Object object, String name, Object... args) {
+        Method method = null;
+        boolean isAccessible = true;
+        try {
+            method = object.getClass().getDeclaredMethod(name, toClasses(args));
+            isAccessible = method.isAccessible();
+            method.setAccessible(true);
+            return method.invoke(object, args);
+        } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        } finally {
+            if (method != null && !isAccessible) {
+                method.setAccessible(false);
+            }
+        }
+        return null;
+    }
+
+    static private Class<?>[] toClasses(Object[] args) {
+        List<Class<?>> classes = Arrays.stream(args)
+                .map(Object::getClass)
+                .collect(Collectors.toList());
+        return classes.toArray(new Class<?>[classes.size()]);
     }
 }
