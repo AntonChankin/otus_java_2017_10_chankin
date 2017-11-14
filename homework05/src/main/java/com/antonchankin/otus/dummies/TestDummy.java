210024827d25fc1a240787723956b1785f251bf2
Anton Chankin <this@antonchankin.com>
2017-11-14 12:29:01 +0300

    Homework 05 adds methods to dummy

--- homework05/src/main/java/com/antonchankin/otus/dummies/TestDummy.java
+++ homework05/src/main/java/com/antonchankin/otus/dummies/TestDummy.java
@@ -1,4 +1,40 @@
 package com.antonchankin.otus.dummies;
 
+import com.antonchankin.otus.annotations.After;
+import com.antonchankin.otus.annotations.Before;
+import com.antonchankin.otus.annotations.Test;
+
 public class TestDummy {
+
+    @Before
+    public boolean beforeMethodAlpha(){
+        boolean isSuccessful = false;
+        return isSuccessful;
+    }
+
+    @Before
+    public boolean beforeMethodBeta(){
+        boolean isSuccessful = false;
+        return isSuccessful;
+    }
+    @Test
+    public boolean testMethodAlpha(){
+        boolean isSuccessful = false;
+        return isSuccessful;
+    }
+    @Test
+    public boolean testMethodBeta(){
+        boolean isSuccessful = false;
+        return isSuccessful;
+    }
+    @After
+    public boolean afterMethodAlpha(){
+        boolean isSuccessful = false;
+        return isSuccessful;
+    }
+    @After
+    public boolean afterMethodBeta(){
+        boolean isSuccessful = false;
+        return isSuccessful;
+    }
 }
