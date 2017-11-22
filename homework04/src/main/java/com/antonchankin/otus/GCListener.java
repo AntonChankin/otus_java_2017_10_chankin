9088f55e1c6d6313da7795eec852cf5a9a00cd71
Anton Chankin <this@antonchankin.com>
2017-11-22 12:26:42 +0300

    Homework 04 saves statistics to csv file

--- homework04/src/main/java/com/antonchankin/otus/GCListener.java
+++ homework04/src/main/java/com/antonchankin/otus/GCListener.java
@@ -2,8 +2,6 @@ package com.antonchankin.otus;
 
 import com.sun.management.GarbageCollectionNotificationInfo;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 import javax.management.Notification;
@@ -19,6 +17,7 @@ import java.lang.management.MemoryUsage;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 public class GCListener implements NotificationListener {
@@ -26,21 +25,19 @@ public class GCListener implements NotificationListener {
     private final int id;
     //keep a count of the total time spent in GCs
     private long totalGcDuration = 0;
-    private File recorder;
-    private FileOutputStream fop = null;
     private String name = "NONE";
+    private FileOutputStream fop = null;
+    private final Object fileLock = new Object();
 
     public GCListener(int id) {
         this.id = id;
         log.info("I'm Listener #" + id);
-        this.recorder = new File("gc_" + id + ".csv");
     }
 
     public GCListener(int id, String name) {
         this.id = id;
         log.info("I'm Listener #" + id);
         this.name = name;
-        this.recorder = new File("gc_" + name + ".csv");
     }
 
     public static void installGCMonitoring(){
@@ -66,6 +63,7 @@ public class GCListener implements NotificationListener {
         if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
             //get the information associated with this notification
             GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+            String gcName = info.getGcName();
             //get all the info and pretty print it
             long duration = info.getGcInfo().getDuration();
             String gcType = info.getGcAction();
@@ -83,6 +81,14 @@ public class GCListener implements NotificationListener {
             //Get the information about each memory space, and pretty print it
             Map<String, MemoryUsage> memBefore = info.getGcInfo().getMemoryUsageBeforeGc();
             Map<String, MemoryUsage> mem = info.getGcInfo().getMemoryUsageAfterGc();
+            long totalUsedBefore = 0;
+            long totalUsedAfter = 0;
+            long totalCommittedBefore = 0;
+            long totalCommittedAfter = 0;
+            long totalMaxBefore = 0;
+            long totalMaxAfter = 0;
+            long totalInitBefore = 0;
+            long totalInitAfter = 0;
             for (Map.Entry<String, MemoryUsage> entry : mem.entrySet()) {
                 String name = entry.getKey();
                 MemoryUsage memdetail = entry.getValue();
@@ -93,12 +99,19 @@ public class GCListener implements NotificationListener {
                 MemoryUsage before = memBefore.get(name);
                 long beforepercent = ((before.getUsed()*1000L)/before.getCommitted());
                 long percent = ((memUsed*1000L)/before.getCommitted()); //>100% when it gets expanded
-
+                totalUsedBefore += before.getUsed();
+                totalUsedAfter += memdetail.getUsed();
+                totalCommittedBefore += before.getCommitted();
+                totalCommittedAfter += memdetail.getCommitted();
+                totalMaxBefore += before.getMax();
+                totalMaxAfter += memdetail.getMax();
+                totalInitBefore += before.getInit();
+                totalInitAfter += memdetail.getInit();
                 log.info("Listener#" + id + ": " + name + (memCommitted==memMax?"(fully expanded)":"(still expandable)") +"used: "+(beforepercent/10)+"."+(beforepercent%10)+"%->"+(percent/10)+"."+(percent%10)+"%("+((memUsed/1048576)+1)+"MB) / ");
             }
             totalGcDuration += info.getGcInfo().getDuration();
             
-            record(gcType,totalGcDuration);
+            record(gcType.substring(0,1),totalGcDuration, gcName, totalUsedBefore, totalUsedAfter, totalCommittedBefore, totalCommittedAfter, totalMaxBefore, totalMaxAfter, totalInitBefore, totalInitAfter);
             long percent = totalGcDuration*1000L/info.getGcInfo().getEndTime();
             log.info("Listener#" + id + ": " + "GC cumulative overhead "+(percent/10)+"."+(percent%10)+"%");
         }
@@ -108,34 +121,65 @@ public class GCListener implements NotificationListener {
     protected void finalize() throws Throwable {
         try {
             if (fop != null) {
+                fop.flush();
                 fop.close();
             }
-        } catch (Exception e) {
-            log.error("Cannot close recorder", e);
+        } catch (IOException ex) {
+            log.error("Cannot close file", ex);
         }
         super.finalize();
     }
 
-    private void record(String gcType, long totalGcDuration) {
-        try {
-            boolean isThereAFile = recorder.exists();
-            if (!isThereAFile) {
-                isThereAFile = recorder.createNewFile();
+    private void record(String gcType, long totalGcDuration, String gcName, long totalUsedBefore, long totalUsedAfter, long totalCommittedBefore, long totalCommittedAfter, long totalMaxBefore, long totalMaxAfter, long totalInitBefore, long totalInitAfter) {
+        Date date = new Date();
+        StringBuilder builder = new StringBuilder();
+        builder.append(date.getTime());
+        builder.append(',');
+        builder.append(gcType);
+        builder.append(',');
+        builder.append(totalGcDuration);
+        builder.append(',');
+        builder.append(totalUsedBefore);
+        builder.append(',');
+        builder.append(totalUsedAfter);
+        builder.append(',');
+        builder.append(totalCommittedBefore);
+        builder.append(',');
+        builder.append(totalCommittedAfter);
+        builder.append(',');
+        builder.append(totalMaxBefore);
+        builder.append(',');
+        builder.append(totalMaxAfter);
+        builder.append(',');
+        builder.append(totalInitBefore);
+        builder.append(',');
+        builder.append(totalInitAfter);
+        String line = builder.toString();
+        appendToFile(line, gcName + ".csv");
+    }
+
+    private void appendToFile(String line, String fileName) {
+        synchronized (fileLock) {
+            try {
+                if (Objects.isNull(fop)) {
+                    File file = new File(fileName);
+                    fop = new FileOutputStream(file);
+                    if (!file.exists()) {
+                        file.createNewFile();
+                    }
+                    fop.write("TIMESTAMP,TYPE,DURATION,USED_BEFORE,USED_AFTER,COMMITTED_BEFORE,COMMITTED_AFTER,MAX_BEFORE,MAX_AFTER,INIT_BEFORE,INIT_AFTER\n".getBytes());
+                }
+                // get the content in bytes
+                if (fop != null) {
+                    line += '\n';
+                    byte[] contentInBytes = line.getBytes();
+                    fop.write(contentInBytes);
+                    fop.flush();
+                    log.debug("Done writing to " + fileName + " line :" + line);
+                }
+            } catch (IOException e) {
+                log.error("Cannot write to file", e);
             }
-            if (isThereAFile) {
-                fop = fop != null ? fop : new FileOutputStream(recorder);
-            } else {
-                log.warn("Cannot write statistics: no file");
-            }
-            if (fop != null) {
-                Date date = new Date();
-                String line = "" + date.getTime() + ',' + gcType + ',' + totalGcDuration;
-                byte[] contentInBytes = line.getBytes();
-                fop.write(contentInBytes);
-                fop.flush();
-            }
-        } catch (IOException e) {
-            log.error("Failed to write statistics", e);
         }
     }
 }
