d99cf32041beee063ba79dfd305d86ae0ac006fd
Anton Chankin <this@antonchankin.com>
2017-11-28 15:39:32 +0300

    Homework 06 Init with quickstart

---
+++ homework06/src/main/java/com/antonchankin/otus/hw06/App.java
@@ -0,0 +1,13 @@
+package com.antonchankin.otus.hw06;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
