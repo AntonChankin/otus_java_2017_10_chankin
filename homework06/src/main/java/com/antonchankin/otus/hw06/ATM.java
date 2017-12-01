ca3ff81611c06da3cb145da8469f0569459c017b
Anton Chankin <this@antonchankin.com>
2017-12-01 09:44:51 +0300

    #7 Implements ATM builder close

--- homework06/src/main/java/com/antonchankin/otus/hw06/ATM.java
+++ homework06/src/main/java/com/antonchankin/otus/hw06/ATM.java
@@ -1,4 +1,16 @@
 package com.antonchankin.otus.hw06;
 
+import com.antonchankin.otus.hw06.api.ConnectionContext;
+import com.antonchankin.otus.hw06.api.WithdrawLogic;
+
 public class ATM {
+    private ConnectionContext context;
+    private WithdrawLogic logic;
+
+    public ATM(ConnectionContext context, WithdrawLogic withdrawLogic) {
+        this.context = context;
+        this.logic = withdrawLogic;
+    }
+
+
 }
--- homework06/src/main/java/com/antonchankin/otus/hw06/util/AtmBuilder.java
+++ homework06/src/main/java/com/antonchankin/otus/hw06/util/AtmBuilder.java
@@ -1,43 +1,65 @@
 package com.antonchankin.otus.hw06.util;
 
 import com.antonchankin.otus.hw06.ATM;
+import com.antonchankin.otus.hw06.api.BankConnector;
+import com.antonchankin.otus.hw06.api.CashDispenser;
+import com.antonchankin.otus.hw06.api.WithdrawLogic;
+import com.antonchankin.otus.hw06.impl.MockBank;
+import com.antonchankin.otus.hw06.impl.BankConnectorContext;
+import com.antonchankin.otus.hw06.impl.CashDispenserImpl;
+import com.antonchankin.otus.hw06.impl.WithdrawSingleton;
 import com.antonchankin.otus.hw06.model.Cartridge;
 
 import java.util.List;
 
 public class AtmBuilder {
-    private ATM atm = new ATM();
+    private String host;
+    private Integer port;
+    private String user;
+    private String password;
+    private List<Cartridge> cartridges;
 
     public AtmBuilder() {
 
     }
 
     public AtmBuilder host(String host) {
-
+        this.host = host;
         return this;
     }
 
     public AtmBuilder user(String user) {
-
+        this.user = user;
         return this;
     }
 
     public AtmBuilder password(String password) {
-
+        this.password = password;
         return this;
     }
 
     public AtmBuilder port(int port) {
-
+        this.port = port;
         return this;
     }
 
     public AtmBuilder cartriges(List<Cartridge> cartridges) {
-
+        this.cartridges = cartridges;
         return this;
     }
 
     public ATM build(){
+        ATM atm = null;
+        if (host != null && port != null && user != null && password != null && cartridges != null) {
+            CashDispenser dispenser = new CashDispenserImpl(cartridges);
+            WithdrawLogic logic = WithdrawSingleton.getInstance();
+            logic.setCashDispenser(dispenser);
+            BankConnector connector = new MockBank();
+            BankConnectorContext context = connector.connectToBank(host, port, user, password);
+            atm = new ATM(context, logic);
+        } else {
+            throw new IllegalStateException("Not initialized");
+        }
         return atm;
     }
 }
